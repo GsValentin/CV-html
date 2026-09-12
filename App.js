24c834a4cc265037f2ff4208aa58b875fb5eadc8
Valentin <georges.valentin.96@gmail.com>
2026-09-12 22:53:18 +0200

    update App.js

--- App.js
+++ App.js
@@ -1,7 +1,12 @@
 const valider = document.querySelector("#valider");
+const message = document.querySelector("#message");
 
 console.log(valider);
 
 valider.addEventListener("click", () => {
   console.log("valider");
 });
+
+valider.addEventListener("click", ()=>{
+  message.textContent = "Valider !!!";
+})
