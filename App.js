e6a8a0e4f229479fcb95a992b84734a68a7d5274
Valentin <georges.valentin.96@gmail.com>
2026-09-15 00:15:52 +0200

    update App.js

--- App.js
+++ App.js
@@ -5,12 +5,26 @@ valider.addEventListener("click", (e) => {
     e.preventDefault();
   
   const texte = article.value.trim();
-  
-  console.log("texte recup :", texte);
-  
-  if(text!==""){
-  console.log("IF")
+  console.log("texte recup :", texte)
+  if(texe!==""){
+  const nouvelElement= document.createElement("li");
+  const checkbox = document.createElement("input");
+  const articles = document.querySelector("#articles");
+  checkbox.type="checkbox";
+
+  const label= document.createElement("label");
+
+  const textArticle= document.createTextNode(texte);
+
+  label.appendChild(checkbox);
+  label.appendChild(textArticle);
+
+  nouvelElement.appendChild(label);
+  articles.appendChild(nouvelElement);
+
+    artice.value= "";
+
 }else{
-    console.log("Else")
+    alert("Aucun article renseinger !!! ");
 }
 });
